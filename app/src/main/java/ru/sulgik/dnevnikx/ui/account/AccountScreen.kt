7ffd87694d223a37503f9c4977897a6de9c8b7c6
vollllodya <nenashkinvov@gmail.com>
2023-03-03 20:20:53 +0700

    fix profile text alignment

--- app/src/main/java/ru/sulgik/dnevnikx/ui/account/AccountScreen.kt
+++ app/src/main/java/ru/sulgik/dnevnikx/ui/account/AccountScreen.kt
@@ -127,6 +127,9 @@ fun Profile(
                 onClick = onSelectAccount,
             )
         ) {
+            Box(
+                modifier = Modifier.size(20.dp)
+            )
             Text(account.name, style = MaterialTheme.typography.bodyLarge)
             Icon(
                 imageVector = Icons.Outlined.ArrowDropDown,
